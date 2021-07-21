4141ee74fcb1751c5eda103e876d4ab20d04192a
nikeusov <nikotin331765115@gmail.com>
2021-07-21 14:30:40 +0300

    init: creat html & js files; all in comments

---
+++ ex.js
@@ -0,0 +1,68 @@
+//  1 Переменные (variables)
+
+// var - variable
+// ctrl + alt + N - запустить JS код (через плагин Code runner)
+// f1 + ввести Run code - запустить JS код (через плагин Code runner)
+// var name = 'Nikita' // устаревшая конструкция;
+// camelCase - название без нижнего подчеркивания, второе слово с большой буквы
+
+const firstName = 'Nikita'
+// const lastName = 'Usov' // константа (не поменять значение); string
+// let age = 24 // числовое занчение без ковычек; number
+const isProgrammer = true // boolean
+
+const _ = 'private' // - можно прописывать код valid
+const $ = 'some val' // - valid
+const withNum5 = '5' // - valid
+// const if = 'ner' - err 
+// const 5withNum = 5 - err
+// console.log(isProgrammer);
+
+// end variables
+
+// 2 Мутирование (mutation)
+
+ // console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age) - мутируем несколько объектов
+ // console.log(age.toString()) - переводим число в строку 
+ // alert('Имя человека: ' + firstName + ', а возраст человека: ' + age) - выод строки не в консоли, а на всплывающем окне при обновлении страницы
+ // prompt('Введите фамилию') - место под запись во всплывающем окне
+
+ // const lastName = prompt('Введите фамилию')
+ // alert(firstName + ' ' + lastName) - введениие новых данных в во всплывающее окно
+
+ // end mutation
+
+ // 3 Операторы (operators)
+
+let currentYear = 2021
+const birthYear = 1996
+
+// const age = currentYear - birthYear
+const a = 10
+const b = 5
+
+let c = 32
+// c = c + a
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(currentYear++)
+// console.log(--currentYear)
+// console.log(c);
+
+// end operators
+
+// 4 Типы данных (data types)
+
+
+
+
+
+
+
